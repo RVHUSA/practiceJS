210bb747e615faa8185965c3c68758e3cae50a47
RVHUSA <rebecca.husa@hotmail.com>
2024-07-10 19:40:30 +0200

    module 1, task 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,62 @@
 console.log("This is script 1")
+
+// question 1
+var integer = 10;
+var decimal = 20.2;
+
+// question 2
+var daysOfWeek = "monday" + "-" + "tuesday" + "-" + "wednesday" + "-" + "thursday" + "-" + "friday"; 
+console.log(daysOfWeek);
+
+// question 3
+var addition = 3 + 4;
+var subtraction = 5 - 3;
+var multiplication = 5 * 4
+var division = 6 / 2 
+
+
+// question 4
+var woorkingToday = false
+
+// question 5
+var sentence = "The frogs are angry";
+console.log(sentence.length);
+
+// question 6 - CamelCase
+var timeElapsed
+var totalPrice
+
+
+// question 7
+var number1 = "53";
+var number2 = "44.6";
+
+var convertNumber1 = parseInt(number1);
+var convertNumber2 = parseFloat(number2);
+
+
+// question 8
+var pet = "donkey";
+var typeOfPet = typeof pet;
+console.log(typeOfPet);
+
+var age = 89;
+var typeOfAge = typeof age;
+console.log(typeOfAge);
+
+var discountApplied = true;
+var typeOfDiscountApplied = typeof discountApplied;
+console.log(typeOfDiscountApplied);
+
+var timeOfTimeLeft = typeof timeLeft;
+console.log(timeOfTimeLeft);
+
+// question 9
+var headingText = "Updating Heading"
+
+var Heading = document.querySelector("h1");
+Heading.innerHTML = headingText;
+
+// question 10
+var body = document.querySelector("body")
+body.style.backgroundColor = "lightskyblue"
